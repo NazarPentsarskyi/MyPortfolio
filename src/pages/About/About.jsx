4e3019917c7a8d79d1c8a1562068bea14cced484
Nazar Pentsarskyi <nazar.pentsarskyi@gmail.com>
2023-05-31 17:27:59 +0300

    added about section

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,7 +3,24 @@ import styles from './styles.module.css';
 export const About = () => {
   return (
     <>
-      <div className={styles.about}>About</div>
+      <div className={styles.about}>
+        <p>
+        Hello!
+        <br />
+        My name is <span>Nazar Pentsarskyi</span>, I am a full-stack CAD/CAM Engineer with 13 years of production experience in 3D modeling, mechanics, and metalworking.
+        <br />
+        Currently, I am working on a volunteer educational project. This project aims to collect data such as articles, posts, photos, 
+        etc., as well as announcements of upcoming events.
+        <br />
+        For the client side, I use <span>React</span> and for the server side, I use <span>Node.js/Express.js</span> to connect to <span>MongoDB</span>.
+        <br />
+        The project is almost completed, and the final stage is design and code improvement.
+        <br />
+        I will be really grateful for the opportunity to join your team as a Junior developer or an intern.
+        <br />
+        See you!
+        </p>
+      </div>
     </>
   )
 }
